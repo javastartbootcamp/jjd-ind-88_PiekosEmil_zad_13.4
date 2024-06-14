86494b3c7f20f8a60ed26efc065ab3cb22d6db6c
Emil Piękoś <piekosemil@gmail.com>
2024-06-14 22:42:40 +0200

    Initial commit.

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -1,6 +1,87 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
-public class CustomList {
+public class CustomList<T> {
+
+    private Object[] objects;
+    private int listSize = 1;
+    private int index;
+
+    public CustomList() {
+        this.objects = new Object[listSize];
+    }
+
+    //Pobieranie elementu po indeksie - get(int index)
+    public Object get(int index) {
+        return objects[index];
+    }
+
+    //Dodawanie elementu (na koniec listy) - add(element)
+    public void add(Object element) {
+        if (listSize == objects.length) {
+            objects = Arrays.copyOf(objects, objects.length * 2);
+        }
+        objects[index] = element;
+        index++;
+        listSize++;
+    }
+
+    //Wstawianie elementu w wybrane miejsce (i przesuwanie pozostałych elementów "dalej") - add(int index, element)
+    public void add(int index, Object element) {
+        for (int i = listSize - 1; i >= index; i--) {
+            if (listSize == objects.length) {
+                objects = Arrays.copyOf(objects, objects.length * 2);
+            }
+            objects[i + 1] = objects[i];
+        }
+        objects[index] = element;
+        listSize++;
+    }
+
+    //Usuwanie elementu po indeksie - remove(int index)
+    public void remove(int index) {
+        objects[index] = null;
+        for (int i = index; i < objects.length; ) {
+            if (objects[i + 1] != null) {
+                objects[i] = objects[i + 1];
+                objects[i + 1] = null;
+                i++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    //Zwracanie informacji o rozmiarze listy - size()
+    public int size() {
+        int counter = 0;
+        for (int i = 0; i < objects.length; i++) {
+            if (objects[i] != null) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    //Wyświetlanie elementów w formacie [element1, element2, element3] - nadpisując metodę toString()
+
+    @Override
+    public String toString() {
+        int i = 0;
+        System.out.print("[");
+        for (Object object : objects) {
+            if (i < size() - 1) {
+                System.out.print(object + ", ");
+            } else if (i <= size() - 1) {
+                System.out.print(object);
+            }
+            i++;
+        }
+        return "]";
+    }
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,8 @@
 package pl.javastart.task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +10,22 @@ public class Main {
         // tutaj możesz przeprowadzać manualne testy listy
 
         // lista powinna być parametryzowana (analogicznie jak java.util.ArrayList czy java.util.LinkedList)
-        CustomList<String> customListOfStrings = new CustomList();
-        CustomList<Integer> customListOfIntegers = new CustomList();
+        CustomList<String> customListOfStrings = new CustomList<>();
+        CustomList<Integer> customListOfIntegers = new CustomList<>();
+        for (int i = 0; i < 10; i++) {
+            customListOfStrings.add("X");
+        }
 
+        customListOfStrings.remove(2);
+        customListOfStrings.add(1, "rex");
+        System.out.println(customListOfStrings);
+        System.out.println("Rozmiar listy: " + customListOfStrings.size());
+
+        for (int i = 1; i < 1000; i *= 2) {
+            customListOfIntegers.add(i);
+        }
+        customListOfIntegers.remove(5);
+        customListOfIntegers.add(5, 777);
+        System.out.println(customListOfIntegers);
     }
 }
